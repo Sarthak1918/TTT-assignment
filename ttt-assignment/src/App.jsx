f5a4717738c945018208398b5af2c511426d55c2
Sarthak1918 <pandasarthak12345@gmail.com>
2023-11-15 01:39:19 +0530

    intial commit
    setup vite and tailwind

---
+++ ttt-assignment/src/App.jsx
@@ -0,0 +1,22 @@
+
+
+function App() {
+
+  return (
+   <div>
+
+{/* navbar */}
+
+    <nav className="bg-black flex px-2 py-4">
+      <div>
+        <img src="" alt="" srcset="" />
+        <p>Stories</p>
+      </div>
+      <button>Courses</button>
+    </nav>
+
+   </div>
+  )
+}
+
+export default App
